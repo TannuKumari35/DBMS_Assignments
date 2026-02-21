5dd287490386308e8020683449c5b969e74e9bab
Tannu Kumari <nitrasingh8@gmail.com>
2026-02-21 14:05:29 +0530

    Update Assignment_1_Solution.sql

--- Assignment_1/Assignment_1_Solution.sql
+++ Assignment_1/Assignment_1_Solution.sql
@@ -1,55 +1,67 @@
+--Create a table Student
 CREATE TABLE Student (
-    RollNo INT,
-    Name VARCHAR2(50),
-    Dept VARCHAR2(20),
-    Age INT,
-    Phone VARCHAR2(15)
+  RollNo INT ,
+  Name VARCHAR(50),
+  Dept VARCHAR(20),
+  Age INT,
+  Phone VARCHAR(15)
 );
 
+
+
+--Create a table Course
 CREATE TABLE Course (
-    CourseID INT,
-    CourseName VARCHAR2(50),
-    Credits INT
+  CourseID INT ,
+  CourseName VARCHAR(50),
+  Credits INT
 );
 
-ALTER TABLE Student ADD City VARCHAR2(30);
+
+--Add a new column City in table Student
+ALTER TABLE Student ADD City VARCHAR(20);
+
+--Add a new column Semester in table Student
 ALTER TABLE Student ADD Semester INT;
 
+--Rename column Phone to MobileNo in table Student.
 ALTER TABLE Student RENAME COLUMN Phone TO MobileNo;
 
+
+--Delete (drop) the table Course                                  
 DROP TABLE Course;
 
 
+--Insert 5 student records into Student
+INSERT INTO Student VALUES (101, 'Naruto Uzumaki', 'CSE', 20, '9876543210', 'Konoha', 5);
+INSERT INTO Student VALUES (102, 'Sasuke Uchiha', 'CSE', 21, '9876543211', 'Konoha', 5);
+INSERT INTO Student VALUES (103, 'Monkey D Luffy', 'IT', 19, '9876543212', 'Foosha', 4);
+INSERT INTO Student VALUES (104, 'Roronoa Zoro', 'ME', 22, '9876543213', 'Shimotsuki', 6);
+INSERT INTO Student VALUES (105, 'Kaneki Ken', 'CSE', 21, '9876543214', 'Tokyo', 5);
 
-INSERT INTO Student VALUES
-(101, 'Rahul', 'CSE', 21, '9876543210', 'Delhi', 5);
 
-INSERT INTO Student VALUES
-(102, 'Aman', 'CSE', 22, '9876543222', 'Patna', 6);
-
-INSERT INTO Student VALUES
-(103, 'Neha', 'ECE', 20, '9876543333', 'Ranchi', 4);
-
-INSERT INTO Student VALUES
-(104, 'Pooja', 'ME', 23, '9876543444', 'Kolkata', 7);
-
-INSERT INTO Student VALUES
-(105, 'Ravi', 'CSE', 19, '9876543555', 'Patna', 3);
-
+--Display all records from Student.
 SELECT * FROM Student;
 
-SELECT RollNo, Name FROM Student;
+--Display only RollNo and Name from Student
+SELECT RollNo , Name FROM Student;
 
-SELECT * FROM Student WHERE Dept = 'CSE';
+--Display all students from department "CSE"                                                                                                              
+SELECT * FROM Student WHERE Dept='CSE';
 
-SELECT * FROM Student WHERE Age > 20;
+--Display all students whose age is greater than 20.                                                                           
+SELECT * FROM Student WHERE Age > 20 ;
 
+--Update the department of RollNo = 101 to "ECE".                              
 UPDATE Student SET Dept = 'ECE' WHERE RollNo = 101;
 
-UPDATE Student SET City = 'Patna' WHERE Name = 'Rahul';
+--Update the city of student "Rahul" to "Patna"
+UPDATE Student SET City = 'Marineford' WHERE Name = 'Monkey D Luffy';
 
-UPDATE Student SET Age = Age + 1;
+--Increase age of all students by 1 year
+UPDATE Student SET Age=Age+1;
 
+--Delete record of student whose RollNo = 105
 DELETE FROM Student WHERE RollNo = 105;
 
+--Delete all records from Student (table should remain)
 DELETE FROM Student;
